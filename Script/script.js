d8e609eee9d786678605bea819bdbdcde9e17e20
4akPow <iljatsapkov11@gmail.com>
2022-04-04 11:32:42 +0300

    Add exchange, Change style

--- Script/script.js
+++ Script/script.js
@@ -21,3 +21,28 @@ function buildCubesList(x, currency){
         list.appendChild(option);
     }
 }
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } 
+);
+
+function myFunction() {
+    var x = document.getElementById("curTo").value;
+    console.log(x)
+    if (document.getElementById("amount")) {
+        var c = document.getElementById("amount").value;
+        var xc = x * c;
+        document.getElementById("curResult").textContent = +xc;
+    }
+}
+
+function loadFlag(element){
+    for(let code in country_list){
+        if(code == element.value){ // if currency code of country list is equal to option value
+            let imgTag = element.parentElement.querySelector("img"); // selecting img tag of particular drop list
+            // passing country code of a selected currency code in a img url
+            imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
+        }
+    }
+}
