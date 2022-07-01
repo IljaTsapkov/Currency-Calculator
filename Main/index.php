372b479cd085c32c7dd77133d1c46a01e87e75f9
4akpow <iljatsapkov11@gmail.com>
2022-07-01 11:47:43 +0300

    Hotfix

--- Main/index.php
+++ Main/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="0; url=https://haldus.eestipank.ee/et/export/currency_rates?imported=2022-03-04&type=xml">
+
     <link rel="stylesheet" type="text/css" href="/Style/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="/Script/script.js"></script>
@@ -14,7 +14,6 @@
     <center>
         <div class="wrapper">
             <h1>Currency Converter</h1><br>
-            <input class="datepicker" name="datepicker" type="date" min="2013-12-25"/><br><br>
             <form id="formCurrency" action="">
                 <label for="amount"> Enter amount</label><br>
                 <input type="text" id="amount" type="number" value="1" pattern="[0-9]"
@@ -25,7 +24,6 @@
                     <div class="select-box">
                         <select name="curFrom" id="curFrom">
                             <option>EUR</option>
-                            <option>LTL</option>
                         </select>
                     </div>
                     <br><br>
